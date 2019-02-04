399a2ce15f408c4b713ccd928e2f728088659d29
Mohin Uddin <csemohin@gmail.com>
2019-02-04 15:38:53 +0600

    Constructor function

---
+++ fncons.js
@@ -0,0 +1,8 @@
+var Myconstructor = function(name,age,job){
+    this.m_name = name;
+    this.m_age = age;
+    this.m_job = job;
+}
+
+var rahim = new Myconstructor("mohin",20,"std");
+console.log(rahim);
